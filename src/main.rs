17c7c5800a02996d4ebad02fe16a75e1cb28fadd
Rojan Rana Magar <rozenmagar058@gmail.com>
2025-03-02 21:15:43 +0545

    feat: added raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    crossterm::terminal::enable_raw_mode().unwrap();
+    for ch in io::stdin().bytes() {
+        let ch = ch.unwrap() as char;
+
+        println!("{}", ch);
+
+        if ch == 'q' {
+            crossterm::terminal::disable_raw_mode().unwrap();
+            break;
+        }
+    }
 }
