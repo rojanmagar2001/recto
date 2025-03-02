15d4418d6998f0fc0104348ff56678252ff1105d
Rojan Rana Magar <rozenmagar058@gmail.com>
2025-03-02 21:22:12 +0545

    chore: added display for keypresses

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ use std::io::{self, Read};
 
 fn main() {
     crossterm::terminal::enable_raw_mode().unwrap();
-    for ch in io::stdin().bytes() {
-        let ch = ch.unwrap() as char;
+    for b in io::stdin().bytes() {
+        let b = b.unwrap();
+        let ch = b as char;
 
-        println!("{}", ch);
+        if ch.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, ch);
+        }
 
         if ch == 'q' {
             crossterm::terminal::disable_raw_mode().unwrap();
