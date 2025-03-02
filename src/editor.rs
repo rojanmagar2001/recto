6868a6ffaa86d55fb4ab69adf6d840d2723e8d7d
Rojan Rana Magar <rozenmagar058@gmail.com>
2025-03-02 21:33:35 +0545

    chore: adding idiomatic way

---
+++ src/editor.rs
@@ -0,0 +1,32 @@
+use std::io::{self, Read};
+
+use anyhow::Context;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn default() -> Self {
+        Self {}
+    }
+
+    pub fn run(&self) -> anyhow::Result<()> {
+        crossterm::terminal::enable_raw_mode().context("couldn't enable raw mode")?;
+        for b in io::stdin().bytes() {
+            let b = b.context("couldn't get the input byte")?;
+            let ch = b as char;
+
+            if ch.is_control() {
+                println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+            } else {
+                println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, ch);
+            }
+
+            if ch == 'q' {
+                crossterm::terminal::disable_raw_mode().context("couldn't disable raw mode")?;
+                break;
+            }
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
-use std::io::{self, Read};
+use editor::Editor;
 
-use anyhow::Context;
+mod editor;
 
 fn main() -> anyhow::Result<()> {
-    crossterm::terminal::enable_raw_mode().context("couldn't enable raw mode")?;
-    for b in io::stdin().bytes() {
-        let b = b.context("couldn't get the input byte")?;
-        let ch = b as char;
+    let editor = Editor::default();
 
-        if ch.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, ch);
-        }
-
-        if ch == 'q' {
-            crossterm::terminal::disable_raw_mode().context("couldn't disable raw mode")?;
-            break;
-        }
-    }
+    editor.run()?;
 
     Ok(())
 }
